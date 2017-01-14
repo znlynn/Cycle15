5af05e4894b958fb5529056afdb9a7ecc7c7eba4
zlynn <znlynn75@gmail.com>
2017-01-14 01:08:48 -0600

    add new intent

---
+++ Splash/app/src/main/java/com/daelabs/zachlynn/splash/ColorGrid.java
@@ -0,0 +1,18 @@
+package com.daelabs.zachlynn.splash;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Zach Lynn on 1/14/2017.
+ */
+
+public class ColorGrid extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.color_grid);
+
+    }
+}
--- Splash/app/src/main/java/com/daelabs/zachlynn/splash/MainActivity.java
+++ Splash/app/src/main/java/com/daelabs/zachlynn/splash/MainActivity.java
@@ -1,13 +1,28 @@
 package com.daelabs.zachlynn.splash;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //private Button play = (Button) findViewById(R.id.btnPlay);
+    //private Button exit = (Button) findViewById(R.id.btnExit);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnPlay).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getBaseContext(), ColorGrid.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
